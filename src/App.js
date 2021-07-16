18e813fc1935d89b01015d2eb002f9931887a7b2
bevanger <baileyevanger@gmail.com>
2021-07-16 11:39:33 -0600

    created state properties, fetched characters from API in an effect hook, mapped over the data, started styling using styled-components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characterData, setCharacterData] =useState([])
+  
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+useEffect(() => {
+  axios.get("https://swapi.dev/api/people")
+    .then(response =>{
+      setCharacterData(response.data);
+      console.log(response);
+      return () => console.log('Cleanup');
+    })
+    .catch(err => console.log(err));
+}, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+    {
+      characterData.map((char) => {
+        return <Character info={char}/>
+      })
+    }  
     </div>
+    
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from "react";
+import styled from 'styled-components';
+
+const StyledCharacters = styled.div`
+    width: 60%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2%;
+    margin: 1% auto;
+    border: 3px solid black;
+
+    font-size: 4rem;
+    font-weight: bold;
+`
+
+export default function Character({info}){
+    return(
+        <StyledCharacters>
+            {info.name}
+        </StyledCharacters>
+    );
+}
